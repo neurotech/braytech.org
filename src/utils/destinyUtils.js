113759c0dbee30723a9b46f1f229f94518a0595a
Tom Chapman <chapman@live.com.au>
2019-02-01 09:49:07 +1000

    stringToIcons()

--- src/utils/destinyUtils.js
+++ src/utils/destinyUtils.js
@@ -1,4 +1,4 @@
-// import { HUNTER, TITAN, WARLOCK, NO_CLASS } from './destinyEnums';
+import React from 'react';
 
 // TODO: we can just use itemCategoryHashes for this now?
 export const isOrnament = item => item.inventory && item.inventory.stackUniqueLabel && item.plug && item.plug.plugCategoryIdentifier && item.plug.plugCategoryIdentifier.includes('skins');
@@ -152,6 +152,48 @@ export function ammoTypeToString(type) {
   }
 
   return string;
+}
+
+function stringToIconsWrapper(string) {
+  return <span className={`destiny-${string}`} />
+}
+
+export function stringToIcons(string) {
+  let array = [];
+
+  let equivalents = {
+    "[Sniper Rifle]": "sniper_rifle",
+    "[Headshot]": "headshot",
+    "[Auto Rifle]": "auto_rifle",
+    "[Pulse Rifle]": "pulse_rifle",
+    "[Scout Rifle]": "scout_rifle",
+    "[Hand Cannon]": "hand_cannon",
+    "[Sidearm]": "sidearm",
+    "[SMG]": "smg",
+    "[Shotgun]": "shotgun",
+    "[Fusion Rifle]": "fusion_rifle",
+    "[Linear Fusion Rifle]": "wire_rifle",
+    "[Trace Rifle]": "beam_weapon",
+    "[Rocker Launcher]": "rocket_launcher",
+    "[Sword]": "sword_heavy",
+    "[Grenade Launcher]": "grenade_launcher"
+  }
+
+  array = string.split(/(\[.*?\])/g);
+
+  array.forEach((part, index) => {
+    let matches = part.match(/\[(.*?)\]/g);
+    if (matches) {
+      matches.forEach(match => {
+        if (!equivalents[match]) {
+          return;
+        }
+        array[index] = stringToIconsWrapper(equivalents[match]);
+      });
+    }
+  });
+
+  return array;
 }
 
 // thank you DIM (https://github.com/DestinyItemManager/DIM/blob/master/src/app/inventory/store/well-rested.ts)
